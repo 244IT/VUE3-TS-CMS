82bb25aa904e8c2a9d3bfc438d1af00d43121c69
244IT <635062564@qq.com>
2021-08-02 15:03:07 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, onMounted, onUnmounted } from "vue"
+
+import HelloWorld from "./components/HelloWorld.vue"
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
 
 <style lang="scss">
